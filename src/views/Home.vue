20190da4b1808e9a4f6c0280c0c3764633138675
Richard Miller <richardmmill@gmail.com>
2021-06-10 11:24:59 -0400

    composition apis and ref for reactivity

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,42 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <p ref="para">My name is {{ name }} and my age is {{ age}}</p>
+    
+    <button @click="handleClick">click</button>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  // runs before other hooks and lifecycle methods
+  // variables in setup are not inherently reactive
+  // adding ref makes variables reactive
+  setup() {
+    console.log('setup')
+
+    const para = ref(null)
+
+    let name = 'Mario'
+    let age = 30
+
+  const handleClick = () => {
+    console.log('you clicked me')
+    console.log(para, para.value)
+    para.value.classList.add('test')
+    para.value.textContent = "hello there"
+  }
+
+    return {name: name, age: age, handleClick, para}
+  }
+  // created() {
+  //   console.log('created')
+  // },
+  // mounted() {
+  //   console.log('mounted')
+  // }
 }
 </script>
